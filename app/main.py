b7ca055fe5f1aa7f8a8c912d1a5f878004ab225e
Florentine <florentine.draeger@protofy.com>
2025-07-25 19:18:18 +0200

    chore: add todo

--- app/main.py
+++ app/main.py
@@ -34,7 +34,7 @@ app = FastAPI(
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Configure this properly for production
+    allow_origins=["*"],  #TODO: Configure this properly for production
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
